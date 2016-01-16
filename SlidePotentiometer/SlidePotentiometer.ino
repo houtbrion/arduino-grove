2bac1b6f5a75e36f06815e415e961c903d670a8d
houtbrion <houtbrion@gmail.com>
2016-01-16 16:25:02 +0900

    スライドボリュームのサンプルプログラムを追加

---
+++ SlidePotentiometer/SlidePotentiometer.ino
@@ -0,0 +1,18 @@
+int adcPin = A0; // select the input pin for the potentiometer
+  int ledPin = A1; // select the pin for the LED
+  int adcIn = 0;   // variable to store the value coming from the sensor
+void setup() 
+{
+  Serial.begin(9600); // init serial to 9600b/s
+  pinMode(ledPin, OUTPUT); // set ledPin to OUTPUT
+  Serial.println("Sliding Potentiometer Test Code!!");
+}
+void loop() 
+{
+  // read the value from the sensor:
+  adcIn = analogRead(adcPin);
+  if(adcIn >= 500) digitalWrite(ledPin,HIGH);  // if adc in > 500, led light
+  else digitalWrite(ledPin, LOW);
+  Serial.println(adcIn);
+  delay(100);
+} 
